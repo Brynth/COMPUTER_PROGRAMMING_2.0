fd4678f7a466421e23d4bd15c20b52f0c4ae89e7
Brynth Dave Gunayan <89186006+Brynth@users.noreply.github.com>
2022-05-17 10:32:47 +0800

    Add files via upload

---
+++ C Array.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main()
+{
+    //a[5]= 0 1 2 3 4 5 ---> 15
+    int a[5],sum=0,i;
+    printf("Enter the size of array :");
+    for(i=0;i<5;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+    printf("sum of all array elements :");
+    for(i=0;i<5;i++)
+    {
+        sum=sum+a[i];
+    }
+    printf("%d",sum);
+    return 0;
+}
